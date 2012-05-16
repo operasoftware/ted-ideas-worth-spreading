50c5130e868497da6ad5a9ed5e86bd624705a56a
Piotr Podgorski <ppodgorski@opera.com>
2012-05-16 14:38:30 +0200

    options page removed, fetching only one latest video added, ted logo enlarged

--- js/main_script.js
+++ js/main_script.js
@@ -1,14 +1,7 @@
 (function() {
 
 	var feeds = [];
-	var feedsToRetrive = 10;
-	var feedsDisplayTime = 5;
 	var feedsLoaded = false;
-		
-	if(widget.preferences.feedsToRetrivePref && widget.preferences.feedsDisplayTimePref){
-		feedsToRetrive = widget.preferences.feedsToRetrivePref;
-		feedsDisplayTime = widget.preferences.feedsDisplayTimePref;
-	}
 	
 	function addClass(elementId, clsName) {
 		document.getElementById(elementId).className = clsName;
@@ -52,30 +45,24 @@
 		
 		document.getElementById("feeds_container").innerHTML = "<span class='ted_banner'><img src='img/stripe.png' /></span>";
 		
-		for(var i = 0; i < feedsToRetrive; i++) {
-			feeds[i] = [];
-			feeds[i][0] = xmlFeeds[i].querySelector("image").getAttribute("url");
-			feeds[i][1] = xmlFeeds[i].querySelector("subtitle").textContent;
-			feeds[i][2] = xmlFeeds[i].querySelector("origLink").textContent;
-			
-			if(feeds[i][0].replace(/\s/g, "") == "") {
-				feeds[i][0] = "img/default.png";
-			}
-			
-			document.getElementById("feeds_container").innerHTML += "<div class='single_feed'><span class='single_feed_img'><img src='" + feeds[i][0] + "' /></span><span class='single_feed_title'>" + feeds[i][1] + "</span></div>";
+		feeds[0] = [];
+		feeds[0][0] = xmlFeeds[0].querySelector("image").getAttribute("url");
+		feeds[0][1] = xmlFeeds[0].querySelector("subtitle").textContent;
+		feeds[0][2] = xmlFeeds[0].querySelector("origLink").textContent;
+		
+		if(feeds[0][0].replace(/\s/g, "") == "") {
+			feeds[0][0] = "img/default.png";
 		}
 		
+		document.getElementById("feeds_container").innerHTML += "<div class='single_feed'><span class='single_feed_img'><img src='" + feeds[0][0] + "' /></span><span class='single_feed_title'>" + feeds[0][1] + "</span></div>";
+	
 		feedsLoaded = true;
 	}
 	
 	document.addEventListener("DOMContentLoaded", function() {
 		getFeedsFromUrl();
-
-		var j = 0;
-		var feedsCntEl = document.getElementById("feeds_container");
-		var reg = new RegExp("(\\s|^)active(\\s|$)");
 		
-		var interval = window.setInterval(function() {
+		window.setTimeout(function() {
 			if(feedsLoaded) {
 				removeClass("error_container", "active");
 				removeClass("loader_container", "active");
@@ -84,70 +71,30 @@
 				feedsLoaded = false;
 			}
 			
-			feedsCntEl.children[j+1].className += " active";
-			
-			if(j == 0 && feedsCntEl.children[feedsToRetrive].className.indexOf("active") != -1) {
-				feedsCntEl.children[feedsToRetrive].className = feedsCntEl.children[feedsToRetrive].className.replace(reg, "");
-			} else {
-				feedsCntEl.children[j].className = feedsCntEl.children[j].className.replace(reg, "");
-			}
-			
-			
 			if (opera.contexts.speeddial) {
-				opera.contexts.speeddial.url = feeds[j][2];
-				//opera.contexts.speeddial.title = "TED: Ideas worth spreading (" + (j + 1) + "/" + feedsToRetrive + ")";
+				opera.contexts.speeddial.url = feeds[0][2];
 			}
-			
-			j++;
-			
-			if(j == feedsToRetrive) {
-				j = 0;
-			}
-		}, feedsDisplayTime * 1000);
+		}, 1500);
 		
-		opera.extension.onmessage = function(event) {
-			if(event.data === "TED_MSG_PREFS_CHANGED") {			
-				if(widget.preferences.feedsToRetrivePref && widget.preferences.feedsDisplayTimePref){
-					feedsToRetrive = widget.preferences.feedsToRetrivePref;
-					feedsDisplayTime = widget.preferences.feedsDisplayTimePref;
-					
-					getFeedsFromUrl();
+		window.setInterval(function() {
+			
+			getFeedsFromUrl();
+			
+			window.setTimeout(function() {
+				if(feedsLoaded) {
+					removeClass("error_container", "active");
+					removeClass("loader_container", "active");
+					removeClass("splash_screen_container", "active");
+					addClass("feeds_container", "active");
+					feedsLoaded = false;
 				}
-			
-				window.clearInterval(interval);
 				
-				j = 0;
-				
-				interval = window.setInterval(function() {
-					if(feedsLoaded) {
-						removeClass("error_container", "active");
-						removeClass("loader_container", "active");
-						removeClass("splash_screen_container", "active");
-						addClass("feeds_container", "active");
-						feedsLoaded = false;
-					}
-					
-					feedsCntEl.children[j+1].className += " active";
-			
-					if(j == 0 && feedsCntEl.children[feedsToRetrive].className.indexOf("active") != -1) {
-						feedsCntEl.children[feedsToRetrive].className = feedsCntEl.children[feedsToRetrive].className.replace(reg, "");
-					} else {
-						feedsCntEl.children[j].className = feedsCntEl.children[j].className.replace(reg, "");
-					}
-					
-					if (opera.contexts.speeddial) {
-						opera.contexts.speeddial.url = feeds[j][2];
-						//opera.contexts.speeddial.title = "TED: Ideas worth spreading (" + (j + 1) + "/" + feedsToRetrive + ")";
-					}
-					
-					j++;
-					
-					if(j == feedsToRetrive) {
-						j = 0;
-					}
-				}, feedsDisplayTime * 1000);
-			}
-		}
+				if (opera.contexts.speeddial) {
+					opera.contexts.speeddial.url = feeds[0][2];
+				}
+			}, 1500);
+		
+		}, 43200000);
 		
 	}, false);
 
